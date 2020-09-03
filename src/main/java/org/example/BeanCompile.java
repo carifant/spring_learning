18a48e4c06acf0cbea106ef6b1b20491c2a4a22b
carifant <carifant@mail.ru>
2020-09-03 20:40:38 +0300

    3 lesson di

--- src/main/java/org/example/BeanCompile.java
+++ src/main/java/org/example/BeanCompile.java
@@ -7,16 +7,12 @@ public class BeanCompile {
     public static void main(String[] args) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("classicBean", Music.class);
-        Music music2 = context.getBean("rockBean", Music.class);
-        Music music3 = context.getBean("reggyBean", Music.class);
 
-
-        PlayerOfMusic playerOfMusic = new PlayerOfMusic(music);
+        PlayerOfMusic playerOfMusic = context.getBean("musicPlayerForClassic", PlayerOfMusic.class);
         playerOfMusic.playMusic();
-        PlayerOfMusic playerOfMusic2 = new PlayerOfMusic(music2);
+        PlayerOfMusic playerOfMusic2 = context.getBean("musicPlayerForRock", PlayerOfMusic.class);
         playerOfMusic2.playMusic();
-        PlayerOfMusic playerOfMusic3 = new PlayerOfMusic(music3);
+        PlayerOfMusic playerOfMusic3 = context.getBean("musicPlayerForReggy", PlayerOfMusic.class);
         playerOfMusic3.playMusic();
         context.close();
     }
